33efa1a2e146428381b73b976b32c4e4e639b813
Foursqweez <jeerasak.code@gmail.com>
2025-04-19 13:47:06 +0700

    Shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,11 +1,15 @@
 
-const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
 
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}");
+    let x = 5;
 
-    println!("{THREE_HOURS_IN_SECONDS}")
+    let x = x + 1; // shadowing
+
+    {
+        let x = x * 2; // shadowing
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
 }
